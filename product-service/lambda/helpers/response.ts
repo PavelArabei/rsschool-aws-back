8bdb7f51651b13a3f5d80a7ce2ed432fc7fbb59b
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-13 13:43:50 +0200

    feat: add response function

---
+++ product-service/lambda/helpers/response.ts
@@ -0,0 +1,19 @@
+import {HTTPHeaderMethods} from "../../types/methods";
+
+export const success = (body: any, allowedMethods?: HTTPHeaderMethods[]) => {
+    return buildResponse(200, body, allowedMethods);
+};
+
+export const failure = (body: any, allowedMethods?: HTTPHeaderMethods[]) => {
+    return buildResponse(500, body, allowedMethods);
+};
+
+export const buildResponse = (statusCode: number, body: any, allowedMethods: HTTPHeaderMethods[] = ["GET"]) => ({
+    statusCode: statusCode,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': allowedMethods.join(","),
+    },
+    body: JSON.stringify(body),
+});
