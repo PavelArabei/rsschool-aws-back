ee5d6369c411e14f1159cc39022e3ebf2cfb6254
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-13 13:46:14 +0200

    fix: using response function instead creating manually

--- product-service/lambda/getProductsById.ts
+++ product-service/lambda/getProductsById.ts
@@ -1,26 +1,18 @@
 import {APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda';
 import {Products} from "../types/products";
+import {buildResponse, success} from "./helpers/response";
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const productId = event.pathParameters?.productId;
-    const products: Products[] = JSON.parse(process.env.PRODUCTS || '[]');
-
-    const product = products.find(p => p.id === productId);
-
-    if (product) {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-            },
-            body: JSON.stringify(product),
-        };
+    if (!productId) {
+        return buildResponse(400, {message: "Product ID is required"})
     }
 
-    return {
-        statusCode: 404,
-        body: JSON.stringify({message: "Product not found"}),
-    };
+    const products: Products[] = JSON.parse(process.env.PRODUCTS || '[]');
+    const product = products.find(p => p.id === productId);
+
+    if (product) return success(product)
+
+    return buildResponse(404, {message: "Product not found"})
+
 };
--- product-service/lambda/getProductsList.ts
+++ product-service/lambda/getProductsList.ts
@@ -1,17 +1,9 @@
 import {APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda';
 import {Products} from "../types/products";
+import {success} from "./helpers/response";
 
 export const handler: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
     const products: Products[] = JSON.parse(process.env.PRODUCTS || '[]');
 
-    return {
-        statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-        },
-        body: JSON.stringify(products),
-    };
-
+    return success(products)
 };
