73b37f8cc3876b7035243f8c40fbc18030b060e1
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-14 13:16:30 +0200

    fix: change getProductsListLambda for dynamoDB

--- product-service/lambda/getProductsList.ts
+++ product-service/lambda/getProductsList.ts
@@ -1,9 +1,38 @@
 import {APIGatewayProxyHandler, APIGatewayProxyResult} from 'aws-lambda';
-import {Products} from "../types/products";
-import {success} from "./helpers/response";
+import {DynamoDB} from 'aws-sdk';
+
+import {buildResponse, failure, success} from "./helpers/response";
+
+
+const {PRODUCTS_TABLE, STOCK_TABLE} = process.env
+const dynamoDb = new DynamoDB.DocumentClient();
 
 export const handler: APIGatewayProxyHandler = async (): Promise<APIGatewayProxyResult> => {
-    const products: Products[] = JSON.parse(process.env.PRODUCTS || '[]');
+    // const products: Products[] = JSON.parse(process.env.PRODUCTS || '[]');
+    try {
+        const productsResult = await dynamoDb.scan({TableName: PRODUCTS_TABLE || ''}).promise();
+        const products = productsResult.Items;
 
-    return success(products)
+        const stocksResult = await dynamoDb.scan({TableName: STOCK_TABLE || ''}).promise();
+        const stocks = stocksResult.Items;
+
+        if (!products || !stocks) {
+            return buildResponse(404, {message: "Products or stocks not found"})
+        }
+
+        const productsWithStocks = products.map(product => {
+            const stock = stocks.find(stock => stock.product_id === product.id);
+            return {
+                ...product,
+                count: stock ? stock.count : 0
+            }
+        });
+
+        return success(productsWithStocks)
+
+    } catch (error) {
+        console.error('Error retrieving products:', error);
+        return failure({error: 'Failed to retrieve products'})
+    }
+
 };
