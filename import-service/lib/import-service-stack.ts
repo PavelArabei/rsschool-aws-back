9243288b15228ff5aa8e9d5667c8e9d885b4534d
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-20 20:48:48 +0200

    feat: add apigw

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as path from "node:path";
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class ImportServiceStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,10 +15,20 @@ export class ImportServiceStack extends cdk.Stack {
             blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
         })
 
+
+        const importProductsFileLambda = new lambda.Function(this, 'ImportProductsFileLambda', {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            handler: 'importProductsFile.handler',
+            code: lambda.Code.fromAsset('src/lambda'),
+            environment: {
+                BUCKET_NAME: bucket.bucketName
+            }
+        });
+
         const importFileParserLambda = new lambda.Function(this, 'ImportFileParserLambda', {
             runtime: lambda.Runtime.NODEJS_16_X,
             handler: 'importFileParser.handler',
-            code: lambda.Code.fromAsset(path.join(__dirname, '../src/lambda')),
+            code: lambda.Code.fromAsset('src/lambda'),
             environment: {
                 BUCKET_NAME: bucket.bucketName,
             },
@@ -26,6 +36,11 @@ export class ImportServiceStack extends cdk.Stack {
 
         bucket.grantPut(importFileParserLambda)
 
+        const api = new apigw.RestApi(this, 'ImportApi', {
+            restApiName: 'Import Service'
+        });
 
+        const importProducts = api.root.addResource('import');
+        importProducts.addMethod('GET', new apigw.LambdaIntegration(importProductsFileLambda))
     }
 }
