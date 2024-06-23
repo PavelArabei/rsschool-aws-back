997bd34ebe3f9de4a8db93d71cf02cd7695be685
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-23 16:16:10 +0200

    feat: create lambda importFileParser.ts

---
+++ import-service/src/lambda/importFileParser.ts
@@ -0,0 +1,34 @@
+import * as AWS from 'aws-sdk';
+import { S3Event, S3Handler } from 'aws-lambda';
+
+const s3 = new AWS.S3();
+const csv = require('csv-parser');
+
+export const handler: S3Handler = async (event: S3Event): Promise<void> => {
+  const bucketName = process.env.BUCKET_NAME!;
+
+  for (const record of event.Records) {
+    const params = {
+      Bucket: bucketName,
+      Key: record.s3.object.key,
+    };
+
+    const s3Stream = s3.getObject(params).createReadStream();
+
+    s3Stream
+      .pipe(csv())
+      .on('data', (data: string) => {
+        console.log(data);
+      })
+      .on('end', async () => {
+        const copyParams = {
+          Bucket: bucketName,
+          CopySource: `${bucketName}/${record.s3.object.key}`,
+          Key: record.s3.object.key.replace('uploaded/', 'parsed/'),
+        };
+
+        await s3.copyObject(copyParams).promise();
+        await s3.deleteObject(params).promise();
+      });
+  }
+};
