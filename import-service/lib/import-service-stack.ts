66099d5c466434acc732261a3e573d34878ee563
Pashtet <ptashkaaaaaa@gmail.com>
2024-06-30 00:06:34 +0200

    feat: add PutCorsConfiguration to existing bucket

--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,27 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import { ImportProductsFileClass } from './constucts/importProductsFileClass';
 import { ImportFileParsedClass } from './constucts/importFileParsedClass';
 import { APIGateWayClass } from './constucts/APIGateWayClass';
+import { ImportBucketClass } from './constucts/importBucketClass';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = s3.Bucket.fromBucketName(this, 'ImportBucket', 'import-service-bucket-rs-school');
+    const { bucket } = new ImportBucketClass(this, 'ImportProductsBucket');
 
     const importProductsFileLambda = new ImportProductsFileClass(this, 'ImportProductsFileLambda', { bucket });
-    const importFileParserLambda = new ImportFileParsedClass(this, 'ImportFileParserLambda', { bucket });
+
+    new ImportFileParsedClass(this, 'ImportFileParserLambda', { bucket });
 
     new APIGateWayClass(this, 'ImportApi', { importProductsFileLambda: importProductsFileLambda.handler });
-
-
-    // importFileParserLambda.addToRolePolicy(
-    //   new iam.PolicyStatement({
-    //     actions: ['s3:PutObject'],
-    //     resources: [`${bucket.bucketArn}/*`],
-    //   })
-    // );
+    
   }
 }
