4563d31518aa86c2eb9e95cebe84df8aa41565df
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-06 20:19:57 +0200

    feat: set a Filter Policy for SNS createProductTopic

--- product-service/lib/constructs/snsTopic.construct.ts
+++ product-service/lib/constructs/snsTopic.construct.ts
@@ -13,7 +13,24 @@ export class SnsTopicConstruct extends Construct {
     this.createProductTopic = new sns.Topic(this, 'CreateProductTopic', {
       topicName: 'createProductTopic1',
     });
-    this.createProductTopic.addSubscription(new subscriptions.EmailSubscription('ptashkaaaaaa@gmail.com'));
+
+    this.createProductTopic.addSubscription(new subscriptions.EmailSubscription('ptashkaaaaaa@gmail.com', {
+        filterPolicy: {
+          price: sns.SubscriptionFilter.numericFilter({
+            greaterThan: 500,
+          }),
+        },
+      }),
+    );
+
+    this.createProductTopic.addSubscription(new subscriptions.EmailSubscription('pavelarabei7788@gmail.com', {
+        filterPolicy: {
+          price: sns.SubscriptionFilter.numericFilter({
+            lessThanOrEqualTo: 500,
+          }),
+        },
+      }),
+    );
 
   }
 }
