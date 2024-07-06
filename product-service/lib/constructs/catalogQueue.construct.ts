67fcaba357ca9413a48dcfb252f27b2f4624b887
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-06 14:55:30 +0200

    feat: create queue construct

---
+++ product-service/lib/constructs/catalogQueue.construct.ts
@@ -0,0 +1,24 @@
+import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { IQueue } from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import { CATALOG_ITEMS_QUEUE_URL } from '../../../constants';
+
+export class CatalogQueueConstruct extends Construct {
+  catalogItemsQueue: IQueue;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.catalogItemsQueue = new sqs.Queue(this, 'CatalogItemsQueue', {
+      queueName: 'catalogItemsQueue',
+    });
+
+    new cdk.CfnOutput(this, 'CatalogItemsQueueUrl', {
+      value: this.catalogItemsQueue.queueUrl,
+      exportName: CATALOG_ITEMS_QUEUE_URL,
+    });
+
+
+  }
+}
