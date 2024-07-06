da8504d4274f7f4de96d85201ad8823bfef118f6
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-06 16:31:44 +0200

    feat: change importProductsFileClass.ts to work with sqs

--- import-service/lib/constucts/importProductsFileClass.ts
+++ import-service/lib/constucts/importProductsFileClass.ts
@@ -1,16 +1,20 @@
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import { IQueue } from 'aws-cdk-lib/aws-sqs';
+
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 interface ImportProductsFileClassProps {
   bucket: s3.IBucket;
+  catalogItemsQueue: IQueue;
 }
 
 export class ImportProductsFileClass extends Construct {
   public readonly handler: lambda.Function;
 
-  constructor(scope: Construct, id: string, { bucket }: ImportProductsFileClassProps) {
+  constructor(scope: Construct, id: string, { bucket, catalogItemsQueue }: ImportProductsFileClassProps) {
     super(scope, id);
+
 
     this.handler = new lambda.Function(this, id, {
       runtime: lambda.Runtime.NODEJS_20_X,
@@ -18,11 +22,14 @@ export class ImportProductsFileClass extends Construct {
       code: lambda.Code.fromAsset('src/lambda'),
       environment: {
         BUCKET_NAME: bucket.bucketName,
+        SQS_URL: catalogItemsQueue.queueUrl,
       },
     });
 
     bucket.grantReadWrite(this.handler);
     bucket.grantPut(this.handler);
+
+    catalogItemsQueue.grantSendMessages(this.handler);
 
   }
 }
