bc7f5e69e1142b500045f4a1427cad14373684f8
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-06 15:12:23 +0200

    feat: add catalogBatchProcessLambda to the lambdasInteractionWithDB.construct.ts

--- product-service/lib/constructs/lambdasInteractionWithDB.construct.ts
+++ product-service/lib/constructs/lambdasInteractionWithDB.construct.ts
@@ -1,15 +1,21 @@
 import { Construct } from 'constructs';
 import { PRODUCTS_TABLE_NAME, STOCKS_TABLE_NAME } from '../conts/constantas';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { IFunction } from 'aws-cdk-lib/aws-lambda';
 
+interface LambdasInteractionWithDBProps {
+  SQS_URL: string;
+  SNS_ARN: string;
+}
 
 export class LambdasInteractionWithDBConstruct extends Construct {
 
-  getProductByIdLambda: lambda.Function;
-  getProductsListLambda: lambda.Function;
-  createProductLambda: lambda.Function;
+  getProductByIdLambda: IFunction;
+  getProductsListLambda: IFunction;
+  createProductLambda: IFunction;
+  catalogBatchProcessLambda: IFunction;
 
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: LambdasInteractionWithDBProps) {
     super(scope, id);
 
     const lambdasEnvironment = {
@@ -37,6 +43,18 @@ export class LambdasInteractionWithDBConstruct extends Construct {
       code: lambda.Code.fromAsset('lambda'),
       handler: 'createProduct.handler',
       environment: lambdasEnvironment,
+    });
+
+
+    this.catalogBatchProcessLambda = new lambda.Function(this, 'CatalogBatchProcessHandler', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'catalogBatchProcess.handler',
+      environment: {
+        ...lambdasEnvironment,
+        SQS_URL: props.SQS_URL,
+        SNS_ARN: props.SNS_ARN,
+      },
     });
 
 
