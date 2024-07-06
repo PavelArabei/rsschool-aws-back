ce33327b05f1ed6036f228bcbe951e7ffde4501f
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-06 18:24:00 +0200

    feat: create catalogBatchProcess.ts lambda

---
+++ product-service/lambda/catalogBatchProcess.ts
@@ -0,0 +1,31 @@
+import { SQSEvent } from 'aws-lambda';
+import { ProductWithoutId } from '../types/product';
+import { createProduct } from './helpers/dynamoDBCommands';
+import { publishSNS } from './helpers/snsCommands';
+import { isProductHasCorrectFields } from './helpers/isProductHasCorrectFields';
+
+export const handler = async (event: SQSEvent) => {
+
+  console.log('request', JSON.stringify(event));
+  const products: ProductWithoutId[] = event.Records.flatMap(record => JSON.parse(record.body));
+  console.log('products from sqs:', products);
+
+  for (const product of products) {
+
+    try {
+
+      if (isProductHasCorrectFields(product)) {
+        throw new Error('Product has incorrect fields');
+      }
+
+      const wholeProduct = await createProduct(product);
+
+      await publishSNS(wholeProduct);
+
+    } catch (error) {
+      console.log('Error while creating product', error);
+    }
+
+  }
+
+};
