326fba92d020ceb5bc4faed22aa9b200df9264bb
Pashtet <ptashkaaaaaa@gmail.com>
2024-07-12 21:36:54 +0200

    feat: integrate  LambdaAuthorizerConstruct in stack

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaAuthorizerConstruct } from './constructs/lambda-authorizer.construct';
 
 export class AuthorizationServiceStack extends cdk.Stack {
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new LambdaAuthorizerConstruct(this, 'LambdaAuthorizerConstruct');
   }
 }
